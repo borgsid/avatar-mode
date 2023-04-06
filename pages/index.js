e6fd35e19897e6f72dd2459ab8d7e7a4db4f503d
borgsid <s-i-l@hotmail.it>
2023-04-06 23:07:03 +0200

    a waorking product

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,140 @@
+import { useState,useEffect } from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
 import buildspaceLogo from '../assets/buildspace-logo.png';
 
-const Home = () => {
-  return (
-    <div className="root">
+  const Home = () => {
+    const maxRetries = 20;
+    // Numbers of retries 
+    const [retry, setRetry] = useState(0);
+    // Number of retries left
+    const [retryCount, setRetryCount] = useState(maxRetries);
+    const [input, setInput] = useState('');
+    const [img, setImg] = useState(''); 
+    const [isGenerating, setIsGenerating] = useState(false);
+    const [finalPrompt, setFinalPrompt] = useState('');
+    const onChange = (event) => {
+      setInput(event.target.value);
+    };
+    const generateAction = async () => {
+      console.log('Generating...');	
+      if (isGenerating && retry === 0) 
+        return;
+      // Set loading has started
+      setIsGenerating(true);
+
+      if (retry > 0) {
+        setRetryCount((prevState) => {
+          if (prevState === 0) {
+            return 0;
+          } else {
+            return prevState - 1;
+          }
+        });
+  
+        setRetry(0);
+      }
+
+      const response = await fetch('/api/generate', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'image/jpeg',
+        },
+        body: JSON.stringify({ input }).replace('sid','borgsidlukee'),
+      });
+
+      const data = await response.json();
+      console.log("data",data)
+      // If model still loading, drop that retry time
+      if (response.status === 503) {
+        console.log('Model is loading still :(.')
+         // Set the estimated_time property in state
+        setRetry(data.estimated_time);
+        return;
+      }
+
+      // If another error, drop error
+      if (!response.ok) {
+        console.log(`Error: ${data.error}`);
+        // Stop loading
+        setIsGenerating(false);
+        return;
+      }
+      // Set final prompt here
+      setFinalPrompt(input);
+      // Remove content from input box
+      setInput('');
+      setImg(data.image);
+       // Everything is all done -- stop loading!
+      setIsGenerating(false);
+    };
+    const sleep = (ms) => {
+      return new Promise((resolve) => {
+        setTimeout(resolve, ms);
+      });
+    };
+    useEffect(() => {
+      const runRetry = async () => {
+        if (retryCount === 0) {
+          console.log(`Model still loading after ${maxRetries} retries. Try request again in 5 minutes.`);
+          setRetryCount(maxRetries);
+          return;
+          }
+  
+        console.log(`Trying again in ${retry} seconds.`);
+  
+        await sleep(retry * 1000);
+  
+        await generateAction();
+      };
+  
+      if (retry === 0) {
+        return;
+      }
+  
+      runRetry();
+    }, [retry]);
+    
+    return (
+      <div className="root">
       <Head>
-        <title>AI Avatar Generator | buildspace</title>
+        <title>Silly picture generator | Borgsid</title>
       </Head>
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>your generator one-liner</h1>
+            <h1>Sydney picture generator</h1>
           </div>
           <div className="header-subtitle">
-            <h2>description of your generator</h2>
+            <h2>
+              Place me anywhere you want, just mention me, "sid" in the prompt
+            </h2>
+          </div>
+          {/* Add prompt container here */}
+          <div className="prompt-container">
+            <input className="prompt-box" value={input} onChange={onChange}   />
+            <div className="prompt-buttons">
+              <a 
+                className={
+                  isGenerating ? 'generate-button loading' : 'generate-button'
+                }
+                onClick={generateAction}>
+                 <div className="generate">
+                    {isGenerating 
+                    ? (<span className="loader"></span>) 
+                    : (<p>Generate</p>)}
+                  </div>
+              </a>
+            </div>
           </div>
         </div>
+        {/* Add output container */}
+        {img && (
+          <div className="output-content">
+            <Image src={img} width={512} height={512} alt={input} />
+            <p>{finalPrompt}</p>
+          </div>
+        )}
       </div>
       <div className="badge-container grow">
         <a
@@ -31,7 +149,7 @@ const Home = () => {
         </a>
       </div>
     </div>
-  );
-};
+    );
+  };
 
 export default Home;
